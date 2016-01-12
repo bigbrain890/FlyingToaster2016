8051543851045a4cc9c19ed808fa20158c95a047
bigbrain890 <bigbrain890@yahoo.com>
2016-01-12 13:14:49 -0500

    Adding Basic Com for Pi Integration
    Added more frame work for I2C bus. Initialized the dataPipe object with
    superficial pi address. Added i2C library.

--- src/org/usfirst/frc/team3641/robot/TrajectoryTracking.java
+++ src/org/usfirst/frc/team3641/robot/TrajectoryTracking.java
@@ -1,8 +1,16 @@
 package org.usfirst.frc.team3641.robot;
 
+import edu.wpi.first.wpilibj.I2C;
+
 public class TrajectoryTracking 
 {
 	private static TrajectoryTracking instance;
+	public static I2C dataPipe;
+	
+	private TrajectoryTracking()
+	{
+		dataPipe = new I2C(I2C.Port.kOnboard, 0x1E);
+	}
 	
 	public static TrajectoryTracking getInstance()
 	{
