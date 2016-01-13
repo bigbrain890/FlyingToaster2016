04bba81f3b01c5e75930e45d87c8489e8c40d447
AlexNeedsAName <Alexander@EpicWebsite.net>
2016-01-13 10:17:22 -0500

    Swiched to 4 CIM Drive
    Commented out all instances of slave motors in the DriveBase class

--- src/org/usfirst/frc/team3641/robot/DriveBase.java
+++ src/org/usfirst/frc/team3641/robot/DriveBase.java
@@ -12,7 +12,7 @@ public class DriveBase
 {
 	private static DriveBase instance;
 	public static CANTalon leftMotor1, leftMotor2, rightMotor1, rightMotor2;
-	public static CANTalon slaveLeft, slaveRight;
+	//public static CANTalon slaveLeft, slaveRight;
 	public static RobotDrive chassis;
 	public static AHRS gyro;
 	public static Encoder driveEncoder;
@@ -21,17 +21,19 @@ public class DriveBase
 	{
 		leftMotor1 = new CANTalon(Constants.LEFT_MOTOR_1);
 		leftMotor2 = new CANTalon(Constants.LEFT_MOTOR_2);
-		slaveLeft = new CANTalon(Constants.LEFT_MOTOR_3);
+		//slaveLeft = new CANTalon(Constants.LEFT_MOTOR_3);
 		rightMotor1 = new CANTalon(Constants.RIGHT_MOTOR_1);
 		rightMotor2 = new CANTalon(Constants.RIGHT_MOTOR_2);
-		slaveRight = new CANTalon(Constants.RIGHT_MOTOR_3);
+		//slaveRight = new CANTalon(Constants.RIGHT_MOTOR_3);
 		gyro = new AHRS(SPI.Port.kMXP);
 		driveEncoder = new Encoder(Constants.DRIVE_ENC_PORT_1, Constants.DRIVE_ENC_PORT_2);
 		chassis = new RobotDrive(leftMotor1, leftMotor2, rightMotor1, rightMotor2);
+		/*
 		slaveRight.changeControlMode(TalonControlMode.Follower);
 		slaveLeft.changeControlMode(TalonControlMode.Follower);
 		slaveRight.set(Constants.RIGHT_MOTOR_1);
 		slaveLeft.set(Constants.LEFT_MOTOR_1);
+		*/
 	}
 	
 	public static DriveBase getInstance()
